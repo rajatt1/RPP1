9b64a13f8908dd9b78d39beac7edf3e132fea37f
rajat1 <rajat.2021@iic.ac.in>
2023-06-21 14:36:18 +0530

    third(route to API)

--- RRPD/project/urls.py
+++ RRPD/project/urls.py
@@ -3,5 +3,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.index, name="index"),
+    path("createProject/", views.projectCreate, name="create_project"),
+    path("save_data/", views.save_data, name="Save_Data"),
 ]
--- RRPD/project/views.py
+++ RRPD/project/views.py
@@ -1,3 +1,29 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
+from http.client import HTTPResponse
+import json
+from django.http import HttpResponse, HttpResponseBadRequest, HttpResponseServerError , JsonResponse
+from django.core import serializers
+from .models import  projectM
+import requests
+from django.template import loader
+from django.views.decorators.csrf import csrf_exempt
+
 
 # Create your views here.
+def projectCreate(request):
+    return render(request,"projectCreate.html")
+
+def save_data(request):
+    if request.method == 'POST':
+        pro_name = request.POST.get('proName')
+        if pro_name:
+            my_projectM = projectM(project_name=pro_name)
+            my_projectM.save()
+
+            serialized_project = serializers.serialize('json', [my_projectM])
+            return redirect('reference' + serialized_project)
+        else:
+            return JsonResponse({'error': 'Invalid data'})
+    else:
+        return JsonResponse({'error': 'Invalid request method'})
+            
